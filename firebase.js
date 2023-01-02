bfc53ff81b01b762553f2acfbc03fc276c5beb8f
go <sukpo6010@gmail.com>
2023-01-02 09:26:52 +0900

    api변경

--- firebase.js
+++ firebase.js
@@ -3,12 +3,13 @@ import { getFirestore } from "firebase/firestore";
 
 // 아래 데이터는 본인의 Firebase 프로젝트 설정에서 확인할 수 있습니다.
 const firebaseConfig = {
-  apiKey: "AIzaSyCLVH96xE91GeHzQIdomwBeekyhXfom6fM",
-  authDomain: "sample12-28823.firebaseapp.com",
-  projectId: "sample12-28823",
-  storageBucket: "sample12-28823.appspot.com",
-  messagingSenderId: "800845592741",
-  appId: "1:800845592741:web:40c68e7860d421444c354c",
+  apiKey: "AIzaSyD6trVgVLj_UzoKmBmZ2pWX44j9ZLgFGqk",
+  authDomain: "nativetodolist.firebaseapp.com",
+  projectId: "nativetodolist",
+  storageBucket: "nativetodolist.appspot.com",
+  messagingSenderId: "11037294657",
+  appId: "1:11037294657:web:5072139462213feb8306c0",
+  measurementId: "G-CRYTSRWK2D",
 };
 
 // Initialize Firebase
